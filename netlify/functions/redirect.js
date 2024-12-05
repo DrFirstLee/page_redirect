e610e2778352a0325b02cb6e458aced3546e0b31
silee <lsi429@naver.com>
2024-12-05 10:16:32 +0900

    how go

--- netlify/functions/redirect.js
+++ netlify/functions/redirect.js
@@ -1,23 +1,18 @@
-// functions/redirect.js
 exports.handler = async function (event, context) {
-    const { headers } = event;
+    const params = event.queryStringParameters || {};
+    const credential = params.credential;
 
-    // 헤더에 'Credential'이 'naver'인지 확인
-    if (headers['credential'] !== 'naver') {
+    if (credential === 'naver') {
+        return {
+            statusCode: 301,
+            headers: {
+                Location: 'https://naver.com/',  // 리디렉션할 URL을 naver.com으로 변경
+            },
+        };
+    } else {
         return {
             statusCode: 403,
-            body: JSON.stringify({ message: 'Unauthorized' })
+            body: 'Unauthorized',
         };
     }
-
-    // 리디렉션을 수행 (naver.com으로)
-    return {
-        statusCode: 301,
-        headers: {
-            'Location': 'https://naver.com/',  // 리디렉션할 URL을 naver.com으로 변경
-            'Access-Control-Allow-Origin': '*',  // 모든 출처에서 접근 허용
-            'Access-Control-Allow-Methods': 'GET',  // GET 메소드 허용
-            'Access-Control-Allow-Headers': 'Credential'  // 'Credential' 헤더 허용
-        },
-    };
 };
